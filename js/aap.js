329cca0d8d7d8943b8f6684e0894de2ad012d684
21fatimakhan <21fatimakhan@gmail.com>
2022-01-31 20:40:26 +0500

    Bakery Website

---
+++ js/aap.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('.food-slider').slick({
+        autoplay:true,
+        slidesToShow:3,
+        slidesToScrolls:1,
+        prevArrow:".prev-btn",
+        nextArrow:".next-btn",
+
+    });
+});
+
+$ (function (){
+    
+})
